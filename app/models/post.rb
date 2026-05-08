4564b7902df5eb035c7d3d539b31583f9689ebb2
kykt35 <kykt35@gmail.com>
2026-05-08 15:18:34 +0900

    Add annotaterb model annotations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id               :integer          not null, primary key
+#  address          :string           not null
+#  area             :string           not null
+#  description      :text
+#  recommended_time :string
+#  status           :string           default("published"), not null
+#  title            :string           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  user_id          :integer          not null
+#
+# Indexes
+#
+#  index_posts_on_area     (area)
+#  index_posts_on_status   (status)
+#  index_posts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
 class Post < ApplicationRecord
   belongs_to :user
   has_many :post_tags, dependent: :destroy
--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: reactions
+#
+#  id            :integer          not null, primary key
+#  reaction_type :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  post_id       :integer          not null
+#  user_id       :integer          not null
+#
+# Indexes
+#
+#  index_reactions_on_post_id                                (post_id)
+#  index_reactions_on_user_id                                (user_id)
+#  index_reactions_on_user_id_and_post_id_and_reaction_type  (user_id,post_id,reaction_type) UNIQUE
+#
+# Foreign Keys
+#
+#  post_id  (post_id => posts.id)
+#  user_id  (user_id => users.id)
+#
 class Reaction < ApplicationRecord
   belongs_to :user
   belongs_to :post
